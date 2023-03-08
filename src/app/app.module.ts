736a26e992924504b5cd509722e40f16cb48e820
Adrien Cheynet <gitcha@outlook.fr>
2023-03-08 19:18:52 +0100

    reorganisation dossier arborcence

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Route, RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TodoAppComponent } from './applications/todo-app/todo-app.component';
 import { MenuAppsComponent } from './menu-apps/menu-apps.component';
-import { TodoListeComponent } from './applications/todo-app/todo-liste/todo-liste.component';
-import { RightClickToolsComponent } from './applications/todo-app/right-click-tools/right-click-tools.component';
-import { NoteComponent } from './applications/todo-app/note/note.component';
-import { FormTodoNoteComponent } from './applications/todo-app/form-todo-note/form-todo-note.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormTodoNoteComponent } from './applications/todo-app/todo-note-liste/form-todo-note/form-todo-note.component';
+import { NoteComponent } from './applications/todo-app/todo-note-liste/note/note.component';
+import { RightClickToolsComponent } from './applications/todo-app/todo-note-liste/right-click-tools/right-click-tools.component';
+import { TodoAppComponent } from './applications/todo-app/todo-note-liste/todo-app.component';
+import { TodoListeComponent } from './applications/todo-app/todo-note-liste/todo-liste/todo-liste.component';
 
 const APP_ROUTES: Route[] = [
   {path:'', component: MenuAppsComponent},
