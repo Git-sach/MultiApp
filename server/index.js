03b997ee356e17845efce22c259d190621d0a007
Adrien Cheynet <gitcha@outlook.fr>
2023-06-05 07:11:23 +0200

    agencement en server - client pour la prod

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const path = require('path');
+
+app.use(morgan('short'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+app.get('*', (req, res) => {
+res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.listen(4000);
